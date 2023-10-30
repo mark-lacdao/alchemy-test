ad7db64c8b289b06dd16b31a5f783ee568d8dcdf
Mark Lacdao <mark.lacdao@gmail.com>
2023-10-30 17:24:53 +0800

    Added status code test

--- Tests/api.py
+++ Tests/api.py
@@ -7,3 +7,6 @@ api_source = "https://jsonplaceholder.typicode.com/users"
 response = requests.get(api_source)
 
 content = json.loads(response.content)
+
+def test_status_code():
+    assert(response.status_code == 200)
