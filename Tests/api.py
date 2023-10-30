6c375763e3af2f751ee0c56f2402892644841227
Mark Lacdao <mark.lacdao@gmail.com>
2023-10-30 18:10:38 +0800

    Added json schema, added validity test

--- Tests/api.py
+++ Tests/api.py
@@ -8,5 +8,14 @@ response = requests.get(api_source)
 
 content = json.loads(response.content)
 
+json_schema_file = "api.schema.json"
+
+schema = {}
+with open(json_schema_file, 'r') as schema_file:
+    schema = json.loads(schema_file.read())
+
+def test_validity():
+    validate(instance=content, schema=schema)
+
 def test_status_code():
     assert(response.status_code == 200)
